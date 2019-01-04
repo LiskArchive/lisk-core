6bab263d8df6ef2449785a323a949116b912cf7b
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-01-04 17:26:19 +0100

    Improve tools network

--- test/tools/network.js
+++ test/tools/network.js
@@ -14,67 +14,80 @@ const getConfigPath = () => {
   return path.resolve(__dirname, configPath);
 }
 
-const enableDisableDelegates = (api, enableOrDisable) => {
-  const configPath = getConfigPath();
-  const configBuffer = fs.readFileSync(configPath);
-  const configContent = JSON.parse(configBuffer);
-  const {
-    nodes,
-    forging: {
-      defaultPassword,
-      delegates,
-    }
-  } = configContent;
+const enableDisableDelegates = (api, isEnable) => {
+  try {
+    const configPath = getConfigPath();
+    const configBuffer = fs.readFileSync(configPath);
+    const configContent = JSON.parse(configBuffer);
+    const enableOrDisable = isEnable ? "enable" : "disable";
+    const {
+      nodes,
+      forging: {
+        defaultPassword,
+        delegates,
+      }
+    } = configContent;
 
-  if (nodes.length === 0) {
-    console.log('\n', '***********Please run npm run tools:peer:config to add nodes to list*************', '\n');
+    if (nodes.length === 0) {
+      console.log('\n', '***********Please run npm run tools:peers:config to add nodes to list*************', '\n');
+      process.exit(1);
+    }
+
+    const chunkSize = Math.ceil(delegates.length / nodes.length);
+    const delegateList = chunkArray(delegates, chunkSize);
+
+    return nodes.map((ip_address, i) => {
+      console.log(`${delegateList[i].length} delegates ${enableOrDisable}d to on node ===> ${ip_address}`, '\n');
+
+      return delegateList[i].map(async (delegate) => {
+        const params = {
+          "forging": isEnable,
+          "password": defaultPassword,
+          "publicKey": delegate.publicKey,
+        };
+
+        console.log(`${enableOrDisable}ing delegate publicKey ===> ${delegate.publicKey} on node ===> ${ip_address}`);
+
+        const { result, error } = await from(api.updateForgingStatus(params, ip_address));
+        expect(error).to.be.null;
+        expect(result.data[0].forging).to.deep.equal(isEnable);
+      });
+    });
+  } catch (error) {
+    console.error(`Failed to ${enableOrDisable} forging due to error: `, error);
     process.exit(1);
   }
-
-  const chunkSize = Math.ceil(delegates.length / nodes.length);
-  const delegateList = chunkArray(delegates, chunkSize);
-
-  return nodes.map((ip_address, i) => {
-    console.log(`${delegateList[i].length} delegates on node==> ${ip_address}`, '\n');
-    return delegateList[i].map(async (delegate) => {
-      const params = {
-        "forging": enableOrDisable,
-        "password": defaultPassword,
-        "publicKey": delegate.publicKey,
-      };
-
-      console.log(`Delegate publicKey==> ${delegate.publicKey}`);
-      const { result, error } = await from(api.updateForgingStatus(params, ip_address));
-
-      expect(error).to.be.null;
-      expect(result.data[0].forging).to.deep.equal(enableOrDisable);
-    });
-  });
 }
 
 Feature('Network tools');
 
-Scenario('Peer list @peer_list', async (I) => {
-  let peers = await I.getAllPeers(100, 0);
-  peers = peers.map(p => p.ip);
-  console.log('Peers list', JSON.stringify(peers, null, '\t'));
+Scenario('Peer list @peers_list', async (I) => {
+  try {
+    const allPeers = await I.getAllPeers(100, 0);
+    console.log('Peers config list: ', JSON.stringify(allPeers, null, '\t'));
+  } catch (error) {
+    console.error('Failed to get peers list: ', error);
+    process.exit(1);
+  }
 });
 
-Scenario('Add peers to config @peer_config', async (I) => {
-  let peers = await I.getAllPeers(100, 0);
-  peers = peers.slice(0, 101).map(p => p.ip);
+Scenario('Add peers to config @peers_config', async (I) => {
+  try {
+    const configPath = getConfigPath();
+    const configBuffer = fs.readFileSync(configPath);
+    const configContent = JSON.parse(configBuffer);
+    const allPeers = await I.getAllPeers(100, 0);
+    const requiredPeers = allPeers.slice(0, 101).map(p => p.ip);
+    const unionNodes = new Set([...configContent.nodes, ...configContent.seed, ...requiredPeers]);
 
-  console.log(`Writing ${peers.length} peers to config file: `, JSON.stringify(peers, null, '\t'));
+    configContent.nodes.push(...unionNodes);
+    fs.writeFileSync(configPath, JSON.stringify(configContent));
 
-  const configPath = getConfigPath();
-  const configBuffer = fs.readFileSync(configPath);
-  const configContent = JSON.parse(configBuffer);
-
-  const unionNodes = new Set([...configContent.nodes, ...configContent.seed, ...peers]);
-
-  configContent.nodes.push(...unionNodes);
-  fs.writeFileSync(configPath, JSON.stringify(configContent));
-  console.log('Updated config!!');
+    console.log(`Updated ${requiredPeers.length} peers to config file: ${configPath}`, JSON.stringify(requiredPeers, null, '\t'));
+  } catch (error) {
+    console.error('Failed to add peers to config: ', error);
+    process.exit(1);
+  }
 });
 
 Scenario('Enable delegates @delegates_enable', async (I) => {
