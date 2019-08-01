ce7964ad88c355991555032754bd39e447136ddf
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-08-01 17:14:48 +0200

    :recycle: Ignore node status check for nodes below 2.0.0

--- qa/step_definitions/node/status.js
+++ qa/step_definitions/node/status.js
@@ -6,21 +6,31 @@ When('I request for node status', async () => {
 	const api = await I.call();
 
 	nodeStatus = await Promise.all(
-		api.peers.map(address => api.getNodeStatus(address))
+		api.peers.map(async address => ({
+			status: api.getNodeStatus(address),
+			address,
+		}))
 	);
 });
 
-Then('I have the status from all the nodes', async () =>
-	nodeStatus.forEach(async res => {
-		await I.expectResponseToBeValid(res, 'NodeStatusResponse');
-	})
-);
+Then('I have the status from all the nodes', async () => {
+	const api = await I.call();
 
-Then('networkHeight should be greater than or equal to height', () => {
-	nodeStatus.forEach(res => {
+	nodeStatus.forEach(async ({ status, address }) => {
+		const nodeConstants = await api.getNodeConstants(address);
+		if (nodeConstants.data.version >= '2.0.0') {
+			const nodeStatusResponse = await status;
+			await I.expectResponseToBeValid(nodeStatusResponse, 'NodeStatusResponse');
+		}
+	});
+});
+
+Then('networkHeight should be greater than or equal to height', async () => {
+	nodeStatus.forEach(async ({ status }) => {
 		if (process.env.NETWORK && process.env.NETWORK !== 'development') {
-			expect(res.data.networkHeight).to.satisfy(
-				nHeight => res.data.height >= nHeight
+			const nodeStatusResponse = await status;
+			expect(nodeStatusResponse.data.networkHeight).to.satisfy(
+				nHeight => nodeStatusResponse.data.height >= nHeight
 			);
 		}
 	});
