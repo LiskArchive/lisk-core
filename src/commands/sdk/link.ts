999b639de11e0c76c508422368a6b377001fd78c
Pablo Vicente <pvicente@gmail.com>
2020-07-27 15:32:27 +0200

    Add sdk:link command

---
+++ src/commands/sdk/link.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Command } from '@oclif/command';
+import { symlinkSync, pathExistsSync, removeSync } from 'fs-extra';
+import { join } from 'path';
+
+export default class LinkCommand extends Command {
+  static description = 'Link an specific SDK folder given the parameter';
+
+  static examples = ['sdk:link', 'sdk:link /sdk/location/'];
+
+  static args = [
+    { name: 'targetSDKFolder' },
+  ]
+
+  // eslint-disable-next-line class-methods-use-this, @typescript-eslint/require-await
+  async run(): Promise<void> {
+    const { args: { targetSDKFolder } } = this.parse(LinkCommand);
+
+    if (targetSDKFolder === undefined) {
+      throw new Error('A path to the location of the SDK folder to be linked needs to be provided.');
+    }
+
+    if (!pathExistsSync(targetSDKFolder)) {
+      throw new Error(`Path '${targetSDKFolder as string}' does not exist or no access allowed`);
+    }
+
+    const sdkLocalPath = join(__dirname, '../../../', 'node_modules', 'lisk-sdk');
+
+    try {
+      removeSync(sdkLocalPath);
+      symlinkSync(targetSDKFolder, sdkLocalPath);
+    } catch (err) {
+      this.error(err);
+    }
+  }
+}
