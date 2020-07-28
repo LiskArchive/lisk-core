f908101283b91ffcea297c5968a162c8a7c0cb98
Pablo Vicente <pvicente@gmail.com>
2020-07-28 10:34:13 +0200

    Apply format

--- test/commands/sdk/link.spec.ts
+++ test/commands/sdk/link.spec.ts
@@ -5,45 +5,49 @@ import { join } from 'path';
 
 describe('sdk:link command', () => {
 	const setupTest = () =>
-    test
-      .stub(fs, 'removeSync', sandbox.stub().returns(null))
-      .stub(fs, 'symlinkSync', sandbox.stub().returns(null))
+		test
+			.stub(fs, 'removeSync', sandbox.stub().returns(null))
+			.stub(fs, 'symlinkSync', sandbox.stub().returns(null))
 			.stdout();
 
-  describe('sdk:link', () => {
-    test
-		.stdout()
-		.command(['sdk:link'])
-		.catch(error => {
-			return expect(error.message).to.contain(
-				'Missing 1 required arg:\ntargetSDKFolder  The path to the lisk SDK folder\nSee more help with --help',
-			);
-		})
-		.it('should throw an error when target folder is missing');
-  });
+	describe('sdk:link', () => {
+		test
+			.stdout()
+			.command(['sdk:link'])
+			.catch(error => {
+				return expect(error.message).to.contain(
+					'Missing 1 required arg:\ntargetSDKFolder  The path to the lisk SDK folder\nSee more help with --help',
+				);
+			})
+			.it('should throw an error when target folder is missing');
+	});
 
-  describe('sdk:link /path/does/not/exist', () => {
-    setupTest()
-    .stub(fs, 'pathExistsSync', sandbox.stub().withArgs('/path/does/not/exist').returns(false))
-    .command(['sdk:link', '/path/does/not/exist'])
-    .catch(error => {
-      return expect(error.message).to.contain('Path \'/path/does/not/exist\' does not exist or no access allowed');
-    })
-    .it('should throw an error when the target folder does not exist');
-  });
+	describe('sdk:link /path/does/not/exist', () => {
+		setupTest()
+			.stub(fs, 'pathExistsSync', sandbox.stub().withArgs('/path/does/not/exist').returns(false))
+			.command(['sdk:link', '/path/does/not/exist'])
+			.catch(error => {
+				return expect(error.message).to.contain(
+					"Path '/path/does/not/exist' does not exist or no access allowed",
+				);
+			})
+			.it('should throw an error when the target folder does not exist');
+	});
 
-  describe('sdk:link /path/exists', () => {
-    const fakeSDKPath = '/path/exists';
-    const targetSDKPath = join(__dirname, '../../../', 'node_modules', 'lisk-sdk');
+	describe('sdk:link /path/exists', () => {
+		const fakeSDKPath = '/path/exists';
+		const targetSDKPath = join(__dirname, '../../../', 'node_modules', 'lisk-sdk');
 
-    setupTest()
-    .stub(fs, 'pathExistsSync', sandbox.stub().withArgs('/path/exists').returns(true))
-    .command(['sdk:link', fakeSDKPath])
-    .it('should call file system functions with correct parameters', out => {
-      expect(fs.pathExistsSync).to.be.calledWith(fakeSDKPath);
-      expect(fs.removeSync).calledWith(targetSDKPath);
-      expect(fs.symlinkSync).calledWith(fakeSDKPath, targetSDKPath);
-      expect(out.stdout).to.contain("Linked '/path/exists' to '/Users/pablo/Documents/LISK/Repos/lisk-core/node_modules/lisk-sdk'");
-    });
-  });
+		setupTest()
+			.stub(fs, 'pathExistsSync', sandbox.stub().withArgs('/path/exists').returns(true))
+			.command(['sdk:link', fakeSDKPath])
+			.it('should call file system functions with correct parameters', out => {
+				expect(fs.pathExistsSync).to.be.calledWith(fakeSDKPath);
+				expect(fs.removeSync).calledWith(targetSDKPath);
+				expect(fs.symlinkSync).calledWith(fakeSDKPath, targetSDKPath);
+				expect(out.stdout).to.contain(
+					"Linked '/path/exists' to '/Users/pablo/Documents/LISK/Repos/lisk-core/node_modules/lisk-sdk'",
+				);
+			});
+	});
 });
