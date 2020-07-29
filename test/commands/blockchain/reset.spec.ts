32d15a9cc8e87cc1e5dbdf32e177d430acde7e55
mitsuaki-u <mitsuaki.uchimoto@gmail.com>
2020-07-29 16:50:09 +0200

    Add test for reset when app is running and no flags

---
+++ test/commands/blockchain/reset.spec.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+import { expect, test } from '@oclif/test';
+import * as sandbox from 'sinon';
+import * as inquirer from 'inquirer';
+import { homedir } from 'os';
+import { join } from 'path';
+import * as appUtils from '../../../src/utils/application';
+import * as dbUtils from '../../../src/utils/db';
+
+const defaultDataPath = join(homedir(), '.lisk', 'default');
+
+describe('blockchain:reset', () => {
+	const pid = 56869;
+	const KVStoreStubInstance = {
+		clear: sandbox.stub(),
+    };
+    const promptStub = sandbox.stub().returns({ answer: false });
+
+	const setupTest = () =>
+		test
+			.stub(dbUtils, 'getBlockchainDB', sandbox.stub().returns(KVStoreStubInstance))
+            .stub(appUtils, 'getPid', sandbox.stub().returns(pid))
+            .stub(inquirer, 'prompt', promptStub)
+			.stdout()
+            .stderr();
+    
+    afterEach(() => {
+        KVStoreStubInstance.clear.resetHistory();
+        promptStub.resetHistory();
+    });
+
+	describe('when application is running', () => {
+		describe('when reset without flags', () => {
+			setupTest()
+				.stub(appUtils, 'isApplicationRunning', sandbox.stub().returns(true))
+				.command(['blockchain:reset'])
+				.catch(error => {
+					expect(error.message).to.equal(
+						`Can't clear db while running application. Application at data path ${defaultDataPath} is running with pid ${pid}.`,
+					);
+				})
+                .it('should log error and return');
+		});
+	});
+});
