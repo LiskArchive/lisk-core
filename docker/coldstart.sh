665e01135be4bc5ad162fea87e3bff1bf6dc282f
François Chavant <francois@lightcurve.io>
2020-09-11 10:01:07 +0200

    remove debug info

--- docker/coldstart.sh
+++ docker/coldstart.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -xe
 
 /home/lisk/node_modules/.bin/lisk-core blockchain:download --network=$1
-ls -l
-file blockchain.db.gz
 /home/lisk/node_modules/.bin/lisk-core blockchain:import --force blockchain.db.gz
