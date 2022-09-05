7e37aa791f41cfd43ffbb8398bb07c2cc09de08a
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2022-09-05 13:58:48 +0200

    Format

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,41 +15,41 @@
  */
 
 module.exports = {
-    apps: [
-        {
-            name: 'lisk-core',
-            script: './bin/run',
-            args: 'start',
-            env: {
-                LISK_NETWORK: "mainnet",
-                LISK_API_IPC: "true",
-            },
-            pid_file: './pids/lisk-core.pid',
-            out_file: './logs/lisk-core.log',
-            error_file: './logs/lisk-core.err',
-            log_date_format: 'YYYY-MM-DD HH:mm:ss SSS',
-        },
-    ],
-    deploy: {
-        server: {
-            "user": "root", // User name of host
-            "host": "8.8.8.8", // IP address of host server
-            "ref": "origin/release/4.0.0", // Deployment branch - should be set to the latest release branch
-            "repo": "https://github.com/LiskHQ/lisk-core.git", // Git repository url
-            "path": "/root/home/temp_deploy", // Deployment directory in server
-            "pre-setup": "rm -r /root/home/temp_deploy", // Executed before repo cloning
-            "post-setup": "source ~/.bashrc && nvm install && npm ci && npm run build", // Executed after repo cloning
-            "post-deploy": "source ~/.bashrc && pm2 start ecosystem.config.js", // Executed on end of deploy command procedures
-        },
-        local: {
-            "user": "root",
-            "host": "127.0.0.1",
-            "ref": "origin/release/4.0.0",
-            "repo": "https://github.com/LiskHQ/lisk-core.git",
-            "path": "/Users/{USER_NAME}/temp_deploy",
-            "pre-setup": "rm -r /Users/{USER_NAME}/temp_deploy",
-            "post-setup": "source ~/.bashrc && source ~/.zshrc && nvm install && npm ci && npm run build",
-            "post-deploy": "source ~/.bashrc && source ~/.zshrc && pm2 start ecosystem.config.js",
-        },
-    }
+	apps: [
+		{
+			name: 'lisk-core',
+			script: './bin/run',
+			args: 'start',
+			env: {
+				LISK_NETWORK: 'mainnet',
+				LISK_API_IPC: 'true',
+			},
+			pid_file: './pids/lisk-core.pid',
+			out_file: './logs/lisk-core.log',
+			error_file: './logs/lisk-core.err',
+			log_date_format: 'YYYY-MM-DD HH:mm:ss SSS',
+		},
+	],
+	deploy: {
+		server: {
+			user: 'root', // User name of host
+			host: '8.8.8.8', // IP address of host server
+			ref: 'origin/release/4.0.0', // Deployment branch - should be set to the latest release branch
+			repo: 'https://github.com/LiskHQ/lisk-core.git', // Git repository url
+			path: '/root/home/temp_deploy', // Deployment directory in server
+			'pre-setup': 'rm -r /root/home/temp_deploy', // Executed before repo cloning
+			'post-setup': 'source ~/.bashrc && nvm install && npm ci && npm run build', // Executed after repo cloning
+			'post-deploy': 'source ~/.bashrc && pm2 start ecosystem.config.js', // Executed on end of deploy command procedures
+		},
+		local: {
+			user: 'root',
+			host: '127.0.0.1',
+			ref: 'origin/release/4.0.0',
+			repo: 'https://github.com/LiskHQ/lisk-core.git',
+			path: '/Users/{USER_NAME}/temp_deploy',
+			'pre-setup': 'rm -r /Users/{USER_NAME}/temp_deploy',
+			'post-setup': 'source ~/.bashrc && source ~/.zshrc && nvm install && npm ci && npm run build',
+			'post-deploy': 'source ~/.bashrc && source ~/.zshrc && pm2 start ecosystem.config.js',
+		},
+	},
 };
