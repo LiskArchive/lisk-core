fee9c75bc78c8e8de21d4f926a4a0b69dea322da
has5aan <has5aan@outlook.com>
2023-08-18 17:20:46 +0200

    Implements getApplication for transaction:create and transaction:sign commands

--- src/commands/transaction/create.ts
+++ src/commands/transaction/create.ts
@@ -1,1 +1,10 @@
-export { TransactionCreateCommand } from 'lisk-commander';
+import { TransactionCreateCommand as BaseTransactionCreateCommand } from 'lisk-commander';
+import { Application, PartialApplicationConfig } from 'lisk-sdk';
+import { getApplication } from '../../application';
+
+export class TransactionCreateCommand extends BaseTransactionCreateCommand {
+	public getApplication(config: PartialApplicationConfig): Application {
+		const app = getApplication(config);
+		return app;
+	}
+}
--- src/commands/transaction/sign.ts
+++ src/commands/transaction/sign.ts
@@ -1,1 +1,10 @@
-export { TransactionSignCommand } from 'lisk-commander';
+import { TransactionSignCommand as BaseTransactionSignCommand} from 'lisk-commander';
+import { Application, PartialApplicationConfig } from 'lisk-sdk';
+import { getApplication } from '../../application';
+
+export class TransactionSignCommand extends BaseTransactionSignCommand {
+	public getApplication(config: PartialApplicationConfig): Application {
+		const app = getApplication(config);
+		return app;
+	}
+}
